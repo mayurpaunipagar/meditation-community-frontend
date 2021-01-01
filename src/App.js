461c9e004e3e546aae167c7eaafdb19d3fb6d309
MayurHpNotebook <001mayur94@gmail.com>
2021-01-01 22:58:24 +0530

    added login buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './images/ashok-chakra.png';
 import './App.css';
-
+import React from 'react';
+import { Button } from 'reactstrap';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Meditation Community</h1>
+        <h1>Meditation Community</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         coming soon ...
+          A platform to connect with all meditators around the world.
         </p>
-        
+        <div className="login">
+            <Button className="btn">Sign In</Button>
+            <Button className="btn">Sign Up</Button>
+        </div>
+
       </header>
+
     </div>
   );
 }
